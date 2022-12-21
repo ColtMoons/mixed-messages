92922be756db31451d32f0045b8a0b086d3d7aa8
Coltmoons <coltmoonsac@gmail.com>
2022-12-20 21:34:27 -0500

    Create all message components

---
+++ main.js
@@ -0,0 +1,9 @@
+ //message Components 
+const pronouns = ['You', 'They', 'We', 'I', 'He', 'She'];
+const verb = ['are', 'aren\'t', 'is', 'isn\'t', 'am', 'am not'];
+const adjetive = ['a good', 'a great', 'a bad', 'the worst', 'the best'];
+const final = ['time', 'day', 'weekend', 'month', 'year'];
+
+const messageComponets = [pronouns, verb, adjetive, final];
+
+//You are having good luck.
